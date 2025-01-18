47a02885089ba1d805e72afc45da81bb7c0d0c7b
charlottegeo <cngg805@gmail.com>
2025-01-18 14:46:53 -0500

    Removed print statements from dbwriter script and added comments

--- db_setup/dbwriter.py
+++ db_setup/dbwriter.py
@@ -14,32 +14,37 @@ dir = "Seasons"
 conn = psycopg2.connect(dbname=DATABASE_NAME, user=USERNAME, password=PASSWORD, host=DATABASE_URL)
 cursor = conn.cursor()
 
-season_dict = {}
+#Note: I know the parsing is weird, but because of the way the folders and files were structured, I thought it was easier to do this rather than renaming all the files
 
+#Loops through all seasons in the directory
 for season in sorted(os.listdir(dir), key=lambda x: int(x.split("S")[1])):
-    season_number = int(season.split("S")[1])
-    print(f"Processing season: {season_number}")
 
+    #Extracts season number
+    season_number = int(season.split("S")[1])
+
+    #Insert season into database and get id
     cursor.execute("INSERT INTO seasons (number) VALUES (%s) RETURNING id;", (season_number,))
     season_id = cursor.fetchone()[0]
-    print(f"Inserted season {season_number} with id {season_id}")
     conn.commit()
 
-    season_dict[season_number] = season_id
-
+    #Loops through all episodes in the season
     for episode in sorted(os.listdir(os.path.join(dir, season)), key=lambda x: int(x.split("E")[1].split("-")[0])):
+
+        #Extracts episode number and title from filename
         episode_number = int(episode.split("E")[1].split("-")[0])
         episode_title = episode.replace(".txt", "").split(" - ")[1]
-        print(f"Processing episode: {episode_number} - {episode_title}")
 
+        #Insert episode into database and get id
         cursor.execute("INSERT INTO episodes (season_id, number, title) VALUES (%s, %s, %s) RETURNING id;", (season_id, episode_number, episode_title))
         episode_id = cursor.fetchone()[0]
-        print(f"Inserted episode {episode_number} with id {episode_id}")
         conn.commit()
 
+        #Loops through all lines in the episode
         with open(os.path.join(dir, season, episode), "r") as f:
                 line_number = 1
                 for line in f.readlines():
+
+                    #Extracts speaker and adds them to the database
                     if ":" in line:
                         speaker, line_content = line.split(":", 1)
                         speaker = speaker.strip()
@@ -47,15 +52,16 @@ for season in sorted(os.listdir(dir), key=lambda x: int(x.split("S")[1])):
                         cursor.execute("SELECT id FROM speakers WHERE name = %s;", (speaker,))
                         speaker_id = cursor.fetchone()[0]
                     else:
+                        #If there is no speaker, set speaker_id to None, still need to figure out what to do with those lines
                         speaker_id = None
 
+                    #Inserts line into database
                     line_content = line_content.strip()
                     print(f"Inserting line {line_number} for episode {episode_number}, speaker {speaker_id}, content: {line_content}")
-
                     cursor.execute("INSERT INTO lines (season_id, episode_id, speaker_id, line_number, content) VALUES (%s, %s, %s, %s, %s);", (season_id, episode_id, speaker_id, line_number, line_content))
                     line_number += 1
+                    
                 conn.commit()
-                print(f"Added episode {episode_number} for season {season_number}")
 
 cursor.close()
 conn.close()
