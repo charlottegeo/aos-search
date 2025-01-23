65fdab972864b9284d7f9b2203b7b563f67e2937
charlottegeo <cngg805@gmail.com>
2025-01-23 05:55:16 -0500

    Removed unnecessary files to transition away from Diesel

--- backend/migrations/2025-01-19-104252_create_schema/down.sql
+++
@@ -1,9 +0,0 @@
-DROP INDEX IF EXISTS idx_lines_speaker_id;
-DROP INDEX IF EXISTS idx_lines_episode_id;
-DROP INDEX IF EXISTS idx_lines_season_id;
-DROP INDEX IF EXISTS idx_episodes_season_id;
-
-DROP TABLE IF EXISTS lines;
-DROP TABLE IF EXISTS speakers;
-DROP TABLE IF EXISTS episodes;
-DROP TABLE IF EXISTS seasons;
